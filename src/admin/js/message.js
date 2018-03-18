d2ff36851f59c0ce3d9df609ee8794db353614f2
贺成龙 <hechenglong@192.168.0.101>
2018-03-18 16:28:44 +0800

    adminlogin

--- src/admin/js/message.js
+++ src/admin/js/message.js
@@ -34,16 +34,9 @@ var message = {
             ,request: {limitName: 'size'}
             ,cols: [[
                 {checkbox: true}, //默认全选,
-                {field: '', title: '姓名', width: 120},
-                {field: 'kefu_name', title: '身份证后六位', width: 120},
-                {field: 'un', title: '毕业年份', width: 100},
-                {field: 'carOwnerName', title: '班级', width: 150},
-                {field: 'licenseNo', title: '所在地区', width: 300,templet: '#titleTpl1'},
-                {field: 'city', title: '单位名称', width: 150},
-                {field: 'insurance_corp', title: '所在部门', width: 150},
-                {field: 'isOver', title: '职务', width: 100,},
-                {field: 'sumPrem', title: '手机号码', width: 150},
-                {field: 'payMode', title: '头像', width: 100},
+                {field: 'addTrueName', title: '发起人姓名', width: 120},
+                {field: 'addTime', title: '发起时间', width: 120},
+                {field: 'topics', title: '内容', width: 300}
             ]],
             page: true
             ,id:'testReload'
