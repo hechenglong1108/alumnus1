5d02494c1f43fd721ae00891e153e9b9ab66442b
贺成龙 <hechenglong@hechenglongdeMacBook-Pro.local>
2018-03-19 15:40:08 +0800

    1

--- src/admin/js/reglist.js
+++ src/admin/js/reglist.js
@@ -19,6 +19,21 @@ var reglist = {
             }
 
         })
+
+        $("#import").click(function(){
+            layer.open({
+                type:2,
+                area:['800px','600px'],
+                title:'导入校友信息',
+                shade:0.6,
+                anim:2,
+                content:'import.html',
+                yes : function(layero,index) {
+                    layer.close(index);
+                }
+            })
+        })
+
         $("#shuaxin").click(function(){
             _inThis.createtable();
         })
@@ -26,7 +41,7 @@ var reglist = {
     createtable: function() {
         table.render({
             elem: '#list' //指定原始表格元素选择器（推荐id选择器）
-            ,url:"/api/dop/self/alumnu/list"
+            ,url:"/api/dop/found/alumnus/list?activationStatus=1"
             ,response: {statusName: 'code',statusCode: 1,countName:'total',dataName:'rows'}
             ,request: {limitName: 'size'}
             ,cols: [[
@@ -38,7 +53,7 @@ var reglist = {
                 {field: 'licenseNo', title: '所在地区', width: 300,templet: '#titleTpl1'},
                 {field: 'company', title: '单位名称', width: 150},
                 {field: 'department', title: '所在部门', width: 150},
-                {field: 'failureNum', title: '职务', width: 100},
+                {field: 'position', title: '职务', width: 100},
                 {field: 'mobile', title: '手机号码', width: 150},
                 {field: 'regionTime', title: '注册时间', width: 100,}
             ]],
