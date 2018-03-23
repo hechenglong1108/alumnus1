c94d45dcf0fe110b69d3d7cea8446b651e6a61f4
贺成龙 <hechenglong@hechenglongdeMacBook-Pro.local>
2018-03-23 16:42:31 +0800

    1

--- src/admin/js/message.js
+++ src/admin/js/message.js
@@ -15,6 +15,36 @@ var message = {
         $("#shuaxin").click(function(){
             _inThis.createtable()
         })
+
+
+        $("#delete").click(function(){
+            if(!Base.isadmin){
+                Base.Messager.open("您的账号不是管理员，请登录管理员账号");
+                setTimeout(function(){
+                    location.href = 'login.html'
+                },2000)
+            }
+            var _selected = table.checkStatus('testReload').data;
+            if(_selected.length != 1) {
+                layer.msg('请选择一条数据。。', {icon: 5});
+                return;
+            }
+            Base.loadJson({
+                url:"/api/dop/topics/del",
+                data:{
+                    tid:_selected[0].tid
+                },
+                type:"post"
+            },function(json){
+                if(json.code*1 == 1 ){
+                    layer.msg(json.message || "删除成功")
+                    _inThis.createtable()
+                }else{
+                    layer.msg(json.message)
+                }
+            })
+        })
+
         $("#update").click(function(){
             if(!Base.isadmin){
                 Base.Messager.open("您的账号不是管理员，请登录管理员账号");
